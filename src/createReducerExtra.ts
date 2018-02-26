cbf5daec5794587fdf1c24a00b2a1ff8892e2013
Dean Merchant <deanmerchant@gmail.com>
2018-02-26 23:22:04 +0000

    Improve DeepReadonly (so it actually does what it claims to do)

--- src/createReducerExtra.ts
+++ src/createReducerExtra.ts
@@ -1,6 +1,10 @@
 import { AnyAction, Reducer } from 'redux'
 
-export type DeepReadonly<S> = { readonly [K in keyof S]: S[K] }
+export type primitive = string | number | boolean | undefined | null
+export type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>
+export type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>
+}
 
 export type Action<T extends string, P> = { type: T, payload: P }
 
