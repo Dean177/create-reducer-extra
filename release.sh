fec30535d523f8d7dc2c9c9c69ed8175e5c205c2
Dean Merchant <deanmerchant@gmail.com>
2018-02-26 23:01:37 +0000

    Leverage new features of Typescript to vastly improve type-safety. Improve readme and add new examples.

--- release.sh
+++ release.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-param=$1
-if [ "x$param" = "x" ]; then
-    param=patch
+VERSION_MODIFIER=$1
+if [ "x$VERSION_MODIFIER" = "x" ]; then
+    VERSION_MODIFIER=patch
 fi
 
 # Create a new tag to keep track of all changes since last release
 COMMIT_MESSAGES=$(git log `git describe --tags --abbrev=0 HEAD^`..HEAD --oneline)
 TAG_MESSAGE=$(printf "\n${COMMIT_MESSAGES}")
-VERSION=$(npm version $param --message "Version %s ${TAG_MESSAGE}")
+VERSION=$(yarn version ${VERSION_MODIFIER} --message "Version %s ${TAG_MESSAGE}")
 
 git push --tags
-npm publish
+yarn publish
